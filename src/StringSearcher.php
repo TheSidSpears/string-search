9461ee1bca79467954de5e1f8d8fee8e74ebd15f
Artur <thesidspears@gmail.com>
2018-11-15 18:46:40 +0300

    add class

--- src/StringSearcher.php
+++ src/StringSearcher.php
@@ -3,5 +3,18 @@
 
 class StringSearcher
 {
+    public function findStingAndPosition(string $filePath, string $needle): ?array
+    {
+        $file = file($filePath);
 
+        foreach ($file as $stringNum => $string) {
+            if ($foundedPosition = mb_strpos($string, $needle)) {
+                return [
+                    'string' => ++$stringNum,
+                    'position' => $foundedPosition
+                ];
+            }
+        }
+        return null;
+    }
 }
